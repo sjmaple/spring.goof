02e7c0a948304b3f5bdf6129248a33090d41714f
Simon Maple <sjmaple@gmail.com>
2018-04-17 17:40:55 +0100

    removed struts vuln

--- src/main/java/org/snyk/groceries/SpringGoofApplication.java
+++ src/main/java/org/snyk/groceries/SpringGoofApplication.java
@@ -1,35 +1,19 @@
 package org.snyk.groceries;
 
-import org.apache.struts2.dispatcher.filter.StrutsPrepareAndExecuteFilter;
 import org.snyk.groceries.domain.Item;
 import org.snyk.groceries.repository.ItemRepository;
-import org.snyk.groceries.services.ItemService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class SpringGoofApplication extends SpringBootServletInitializer implements ApplicationRunner{
-
-	@Autowired
-    private ItemService itemService;
+public class SpringGoofApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringGoofApplication.class, args);
-	}
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	    return application.sources(SpringGoofApplication.class);
 	}
 	
 	@Bean
@@ -56,18 +40,4 @@ public class SpringGoofApplication extends SpringBootServletInitializer implemen
 			repository.findByName("Bread").forEach(System.out::println);
 			};
 		};
-		
-		@Bean
-	    public FilterRegistrationBean filterRegistrationBean() {
-	        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-	        StrutsPrepareAndExecuteFilter struts = new StrutsPrepareAndExecuteFilter();
-	        registrationBean.setFilter(struts);
-	        registrationBean.setOrder(1);
-	        return registrationBean;
-	    }
-	 
-	    @Override
-	    public void run(ApplicationArguments args) throws Exception {
-	        itemService.getAllItems();
-	    }
 }
